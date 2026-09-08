3c16f02f619f830d8b944fe0137d12ea968002d2
denefha-dev <denefha@gmail.com>
2026-09-08 22:53:00 +0700

    update

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+-- memunculkan file hidden (.gitignore)
+
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
+}
