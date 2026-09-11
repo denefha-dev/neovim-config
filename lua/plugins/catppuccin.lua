469980c7230271980904803b9cf832beaa99a09b
denefha-dev <denefha@gmail.com>
2026-09-11 18:53:12 +0700

    catppuccin.lua
    menjadikan color scheme catppuccin sebagai utama dan meng-config neovim
    menjadi transparat

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,14 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      flavour = "mocha",
+      transparent_background = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
     },
   },
 }
