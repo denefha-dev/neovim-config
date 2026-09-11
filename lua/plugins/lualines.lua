e0ccbe828f9cb5159d8454fea8bd51b9587c03ff
denefha-dev <denefha@gmail.com>
2026-09-11 19:58:53 +0700

    lualines.lua
    update lualines ---> melihat panjang syntax/code pada file

--- lua/plugins/lualines.lua
+++ lua/plugins/lualines.lua
@@ -3,7 +3,7 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, 1, {
+      table.insert(opts.sections.lualine_z, 1, {
         function()
           return vim.api.nvim_buf_line_count(0) .. " lines"
         end,
