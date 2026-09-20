48b365080bdc116ae04d8ac8825b4f4de550d30a
denefha-dev <denefha@gmail.com>
2026-09-21 01:42:01 +0700

    snacks.lua
    update snacks.lua supaya bisa merender gambar dengan format .png dll,
    saat file di pilih atau di choose

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,11 +1,17 @@
 -- memunculkan file hidden (.gitignore)
 -- ubah nama dashboard menjadi (denefha)
+-- tampilkan preview file dan gambar di Snacks Picker
 
 return {
   {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        layout = {
+          preset = "default",
+          preview = true,
+        },
+
         sources = {
           explorer = {
             hidden = true,
